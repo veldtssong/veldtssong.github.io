f6821684b905fe82480a0a1d54d51abd5d99153e
haydnvn <haydnavn@gmail.com>
2024-07-06 14:14:23 -0400

    campaign edit

--- generateImagesJson.js
+++ generateImagesJson.js
@@ -24,3 +24,28 @@ fs.readdir(directory, (err, files) => {
     }
   });
 });
+
+
+const directory2 = './pictures/Campaign';  // Adjust directory path as necessary
+
+fs.readdir(directory2, (err, files) => {
+    if (err) {
+      console.error('Error reading directory:', err);
+      return;
+    }
+  
+    const imagePaths = files.filter(file => file.endsWith('.jpeg') || file.endsWith('.jpg') || file.endsWith('.png'))
+                             .map(file => path.join(directory2, file));
+  
+    const jsonData = {
+      images: imagePaths
+    };
+  
+    fs.writeFile('campaign.json', JSON.stringify(jsonData, null, 2), err => {
+      if (err) {
+        console.error('Error writing JSON file:', err);
+      } else {
+        console.log('JSON file has been generated successfully.');
+      }
+    });
+  });
