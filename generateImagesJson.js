1e44b56c711f7098d5a4a03ab1bf163499ed71d6
haydnvn <haydnavn@gmail.com>
2024-07-06 15:16:39 -0400

    lookbook

--- generateImagesJson.js
+++ generateImagesJson.js
@@ -49,3 +49,28 @@ fs.readdir(directory2, (err, files) => {
       }
     });
   });
+
+  const directory3 = './pictures/Lookbook';  // Adjust directory path as necessary
+
+fs.readdir(directory3, (err, files) => {
+    if (err) {
+      console.error('Error reading directory:', err);
+      return;
+    }
+  
+    const imagePaths = files.filter(file => file.endsWith('.jpeg') || file.endsWith('.jpg') || file.endsWith('.png'))
+                             .map(file => path.join(directory3, file));
+  
+    const jsonData = {
+      images: imagePaths
+    };
+  
+    fs.writeFile('campaign.json', JSON.stringify(jsonData, null, 2), err => {
+      if (err) {
+        console.error('Error writing JSON file:', err);
+      } else {
+        console.log('JSON file has been generated successfully.');
+      }
+    });
+  });
+
